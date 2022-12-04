1a3bf44e7139f211f5aae9e72a73aa7350aea9e6
garaiza-93 <araizagustavo93@gmail.com>
2022-12-03 18:36:12 -0600

    begin using flakes

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      nixosConfigurations = {
+        EVA-01 = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./machines/EVA-01/EVA-01.nix
+            ./env/nvidia.nix
+            ./ui/x11/xserver/EVA-01.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.goose = { ... }: {
+                imports = [ ./profiles/goose.nix ];
+              };
+            }
+          ];
+        };
+      };
+    };
+}
