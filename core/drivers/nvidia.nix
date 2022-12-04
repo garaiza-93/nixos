ad896cc0084803fa2913bb71ad7269dc770575bb
garaiza-93 <araizagustavo93@gmail.com>
2022-12-04 00:38:41 -0600

    fixing bugs

--- core/drivers/nvidia.nix
+++ core/drivers/nvidia.nix
@@ -9,7 +9,6 @@
       enable = true;
       driSupport32Bit = true;
     };
-    nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
     nvidia.forceFullCompositionPipeline = true;
   };
 
