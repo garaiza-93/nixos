daefd0a3328d1813a9f940798baed6d8c041860f
garaiza-93 <araizagustavo93@gmail.com>
2022-12-03 22:44:40 -0600

    configure xserver for EVA-01, broken i3 config

---
+++ ui/x11/EVA-01.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }:
+
+{
+  services.xserver = {
+    enable = true;
+    config = ''
+      Section "Screen"
+        Identifier      "Screen0"
+        Device          "Device0"
+        Monitor         "Monitor0"
+        DefaultDepth    24
+        Option          "Stereo" "0"
+        Option          "nvidiaXineramaInfoOrder" "DFP-4"
+        Option          "metamodes" "2560x1440_144 +0+0"
+        Option          "SLI" "Off"
+        Option          "MultiGPU" "off"
+        Option          "BaseMosaic" "off"
+        SubSection      "Display"
+            Depth       24
+        EndSubSection
+      EndSection
+    '';
+  };
+}
---
+++ ui/x11/i3.nix
@@ -0,0 +1,78 @@
+{ pkgs, lib, ... }:
+{
+  xsession.windowManager.i3 = {
+    enable = true;
+    package = pkgs.i3-gaps;
+    config = rec {
+      modifier = "Mod1";
+      menu = "${pkgs.dmenu}/bin/dmenu_run";
+      terminal = "${pkgs.kitty}/bin/kitty";
+      bars = [
+        {
+          id = "bar-1";
+          position = "top";
+          fonts = {
+            names = [ "Hack Nerd Font" "FontAwesome" ];
+            size = 8.0;
+          };
+          statusCommand = "i3status";
+        }
+      ];
+
+      window.border = 0;
+
+      gaps = {
+        inner = 5;
+        outer = 5;
+      };
+
+      keybindings = lib.mkOptionDefault {
+        "XF86AudioRaiseVolume" = "exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ +10% && $refresh_i3status";
+        "XF86AudioLowerVolume" = "exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ -10% && $refresh_i3status";
+        "XF86AudioMute" = "exec --no-startup-id pactl set-sink-mute @DEFAULT_SINK@ toggle && $refresh_i3status";
+        "XF86AudioMicMute" = "exec --no-startup-id pactl set-source-mute @DEFAULT_SOURCE@ toggle && $refresh_i3status";
+
+        "Super_L" = "exec --no-startup-id ${menu} -i";
+        "Print" = "exec flameshot gui";
+        "${modifier}+Shift+q" = "kill";
+        "${modifier}+Return" = "exec ${terminal}";
+
+        "${modifier}+h" = "focus left";
+        "${modifier}+j" = "focus down";
+        "${modifier}+k" = "focus up";
+        "${modifier}+l" = "focus right";
+        "${modifier}+Shift+h" = "move left";
+        "${modifier}+Shift+j" = "move down";
+        "${modifier}+Shift+k" = "move up";
+        "${modifier}+Shift+l" = "move right";
+
+        "${modifier}+z" = "split h";
+        "${modifier}+x" = "split v";
+
+      };
+
+      modes = {
+        resize = {
+          "h" = "resize shrink width 10 px or 10 ppt";
+          "j" = "resize grow height 10 px or 10 ppt";
+          "k" = "resize shrink height 10 px or 10 ppt";
+          "l" = "resize grow width 10 px or 10 ppt";
+          "r" = "mode default";
+        };
+      };
+
+      focus = {
+        followMouse = false;
+      };
+
+      floating = {
+        modifier = "Mod1";
+      };
+    };
+
+    extraConfig = ''
+      set $refresh_i3status killall -SIGUSR1 i3status
+      tiling_drag modifier titlebar
+    '';
+  };
+}
