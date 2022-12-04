26669f6df81d0ec8c27428e24cb67ffa57b460b9
garaiza-93 <araizagustavo93@gmail.com>
2022-12-04 13:37:40 -0600

    fix font, add border, slightly bigger font size, fix resize binds, workspace variables added

--- ui/x11/i3.nix
+++ ui/x11/i3.nix
@@ -3,27 +3,38 @@
 {
   xsession.windowManager.i3 = {
     enable = true;
+
     config = rec {
       modifier = "Mod1";
+
       menu = "${pkgs.dmenu}/bin/dmenu_run";
       terminal = "${pkgs.kitty}/bin/kitty";
+
+      fonts = {
+        names = [ "Hack Nerd Font" ];
+        size = 8.0;
+      };
+
       bars = [
         {
           id = "bar-1";
           position = "top";
           fonts = {
-            names = [ "Hack Nerd Font" "FontAwesome" ];
-            size = 8.0;
+            names = [ "Hack Nerd Font" ];
+            size = 8.5;
           };
           statusCommand = "i3status";
         }
       ];
 
-      window.border = 0;
+      window.border = 1;
 
-      gaps = {
-        inner = 5;
-        outer = 5;
+      defaultWorkspace = "$ws1";
+      workspaceAutoBackAndForth = true;
+
+      assigns = {
+        "1: web" = [{ class = "^Firefox$"; }];
+        "2: games" = [{ class = "^Steam$"; }];
       };
 
       keybindings = lib.mkOptionDefault {
@@ -49,14 +60,25 @@
         "${modifier}+z" = "split h";
         "${modifier}+x" = "split v";
 
+        "${modifier}+1" = "workspace $ws1";
+        "${modifier}+2" = "workspace $ws2";
+        "${modifier}+3" = "workspace $ws3";
+        "${modifier}+4" = "workspace $ws4";
+        "${modifier}+5" = "workspace $ws5";
+        "${modifier}+6" = "workspace $ws6";
+        "${modifier}+7" = "workspace $ws7";
+        "${modifier}+8" = "workspace $ws8";
+        "${modifier}+9" = "workspace $ws9";
+        "${modifier}+0" = "workspace $ws10";
+
       };
 
       modes = {
         resize = {
-          "h" = "resize shrink width 10 px or 10 ppt";
-          "j" = "resize grow height 10 px or 10 ppt";
-          "k" = "resize shrink height 10 px or 10 ppt";
-          "l" = "resize grow width 10 px or 10 ppt";
+          "h" = "resize grow width 10 px or 10 ppt";
+          "j" = "resize shrink height 10 px or 10 ppt";
+          "k" = "resize grow height 10 px or 10 ppt";
+          "l" = "resize shrink width 10 px or 10 ppt";
           "r" = "mode default";
         };
       };
@@ -73,6 +95,16 @@
     extraConfig = ''
       set $refresh_i3status killall -SIGUSR1 i3status
       tiling_drag modifier titlebar
+      set $ws1 "1"
+      set $ws2 "2"
+      set $ws3 "3"
+      set $ws4 "4"
+      set $ws5 "5"
+      set $ws6 "6"
+      set $ws7 "7"
+      set $ws8 "8"
+      set $ws9 "9"
+      set $ws10 "10"
     '';
   };
 }
