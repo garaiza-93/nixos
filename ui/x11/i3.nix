3f70b743c7947bd5a2ec66c0e5ba09b0f42b66cb
garaiza-93 <araizagustavo93@gmail.com>
2022-12-04 16:56:44 -0600

    override more keybinds, use rofi menu

--- ui/x11/i3.nix
+++ ui/x11/i3.nix
@@ -36,7 +36,7 @@
         "XF86AudioMute" = "exec --no-startup-id pactl set-sink-mute @DEFAULT_SINK@ toggle && $refresh_i3status";
         "XF86AudioMicMute" = "exec --no-startup-id pactl set-source-mute @DEFAULT_SOURCE@ toggle && $refresh_i3status";
 
-        "Super_L" = "exec --no-startup-id ${menu} -i";
+        "Super_L" = "exec --no-startup-id ${menu} -show run";
         "Print" = "exec flameshot gui";
         "${modifier}+Shift+q" = "kill";
         "${modifier}+Return" = "exec ${terminal}";
@@ -53,17 +53,27 @@
         "${modifier}+z" = "split h";
         "${modifier}+x" = "split v";
 
-        "${modifier}+1" = "workspace $ws1";
-        "${modifier}+2" = "workspace $ws2";
-        "${modifier}+3" = "workspace $ws3";
-        "${modifier}+4" = "workspace $ws4";
-        "${modifier}+5" = "workspace $ws5";
-        "${modifier}+6" = "workspace $ws6";
-        "${modifier}+7" = "workspace $ws7";
-        "${modifier}+8" = "workspace $ws8";
-        "${modifier}+9" = "workspace $ws9";
-        "${modifier}+0" = "workspace $ws10";
+        "${modifier}+1" = "workspace $ws0";
+        "${modifier}+2" = "workspace $ws1";
+        "${modifier}+3" = "workspace $ws2";
+        "${modifier}+4" = "workspace $ws3";
+        "${modifier}+5" = "workspace $ws4";
+        "${modifier}+6" = "workspace $ws5";
+        "${modifier}+7" = "workspace $ws6";
+        "${modifier}+8" = "workspace $ws7";
+        "${modifier}+9" = "workspace $ws8";
+        "${modifier}+0" = "workspace $ws9";
 
+        "${modifier}+Shift+1" = "move container to workspace $ws0";
+        "${modifier}+Shift+2" = "move container to workspace $ws1";
+        "${modifier}+Shift+3" = "move container to workspace $ws2";
+        "${modifier}+Shift+4" = "move container to workspace $ws3";
+        "${modifier}+Shift+5" = "move container to workspace $ws4";
+        "${modifier}+Shift+6" = "move container to workspace $ws5";
+        "${modifier}+Shift+7" = "move container to workspace $ws6";
+        "${modifier}+Shift+8" = "move container to workspace $ws7";
+        "${modifier}+Shift+9" = "move container to workspace $ws8";
+        "${modifier}+Shift+0" = "move container to workspace $ws9";
       };
 
       modes = {
@@ -88,6 +98,7 @@
     extraConfig = ''
       set $refresh_i3status killall -SIGUSR1 i3status
       tiling_drag modifier titlebar
+      set $ws0 "0"
       set $ws1 "1"
       set $ws2 "2"
       set $ws3 "3"
@@ -97,7 +108,6 @@
       set $ws7 "7"
       set $ws8 "8"
       set $ws9 "9"
-      set $ws10 "10"
     '';
   };
 }
