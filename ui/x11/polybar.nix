373a6cb382507925937849c67cd1a1e36d78e0ba
garaiza-93 <araizagustavo93@gmail.com>
2022-12-04 19:32:14 -0600

    basic bar

--- ui/x11/polybar.nix
+++ ui/x11/polybar.nix
@@ -3,6 +3,35 @@
 {
   services.polybar = {
     enable = true;
-    script = "polybar example &";
+
+    config = {
+      "bar/top" = {
+        width = "100%";
+        height = "34";
+
+        background = "#00000000";
+        foreground = "#ccffffff";
+
+        font-0 = "Hack:size-8";
+
+        modules-left = "i3";
+        modules-right = "wireless-network date";
+
+      };
+
+      "module/date" = {
+        type = "internal/date";
+        internal = 1;
+        date = "%d.%m.%y";
+        time = "%H:%M:%S";
+        label = "%date% %time%";
+      };
+    };
+
+    script = ''
+      #!/usr/bin/env sh
+      killall polybar
+      polybar top &
+    '';
   };
 }
