a3ee980b7c5eff341318e2ad03afeda6f670db59
garaiza-93 <araizagustavo93@gmail.com>
2022-12-11 21:32:01 -0600

    background blurs

--- ui/x11/picom.nix
+++ ui/x11/picom.nix
@@ -6,5 +6,19 @@
     fade = true;
     fadeDelta = 2;
     backend = "glx";
+
+    #opacityRules = [];
+
+    settings = {
+      blur-kern = "5x5box";
+      blur = {
+        method = "dual_kawase";
+        strength = 5;
+        background = false;
+        background-frame = false;
+        background-fixed = false;
+        kern = "5x5box";
+      };
+    };
   };
 }
