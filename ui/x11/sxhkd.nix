639110ac3b30578401b8f636ec95ad69883ed798
garaiza-93 <araizagustavo93@gmail.com>
2022-12-11 19:01:29 -0600

    volume keybinds

--- ui/x11/sxhkd.nix
+++ ui/x11/sxhkd.nix
@@ -21,6 +21,11 @@ in
       "alt + {_,shift +}{h,j,k,l}" = "bspc node --{focus,swap} {west,south,north,east}";
       "alt + {_,shift +}{1-5}" = "bspc {desktop -f,node -d} {1-5}";
       "alt + f" = "bspc node -t \~fullscreen";
+
+      "XF86AudioRaiseVolume" = "pactl set-sink-volume @DEFAULT_SINK@ +5%";
+      "XF86AudioLowerVolume" = "pactl set-sink-volume @DEFAULT_SINK@ -5%";
+      "XF86AudioMute" = "pactl set-sink-mute @DEFAULT_SINK@ toggle";
+      "XF86AudioMicMute" = "pactl set-source-mute @DEFAULT_SOURCE@ toggle";
     };
   };
 }
