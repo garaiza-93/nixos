7b9dd47f46da4e8802cdb5c79b3ee325fdcdac51
garaiza-93 <araizagustavo93@gmail.com>
2022-12-15 21:14:32 -0600

    finish theme and layout

--- ui/x11/rofi.nix
+++ ui/x11/rofi.nix
@@ -15,39 +15,36 @@ in
   programs.rofi = {
     enable = true;
 
-    font = "Hack Nerd Font 14";
+    font = "Hack Nerd Font 12";
     location = "center";
     terminal = "${pkgs.kitty}/bin/kitty";
 
     theme = {
       window = {
         background-color = mkLiteral "transparent";
-        border = 1;
         border-color = mkLiteral "${background-alt}";
+        border = 1;
 
+        width = 900;
+
+        expand = false;
         orientation = mkLiteral "horizontal";
         spacing = 0;
-        children = mkLiteral "[ vertbox, searchbox ]";
+        children = mkLiteral "[ textboxtips, searchbox ]";
       };
 
-      vertbox = {
-        background-color = mkLiteral "transparent";
-
-        orientation = mkLiteral "vertical";
-        children = mkLiteral "[ textbox ]";
-      };
-
-      textbox = {
+      textboxtips = {
         background-color = mkLiteral "${background}";
         color = mkLiteral "${foreground}";
         border-color = mkLiteral "${blue_accent}";
         border = 1;
 
         padding = 5;
-        expand = true;
+        expand = false;
         markup = true;
-        content = "<span size=\"large\"><b>hello world</b></span>\\ngoodbye world";
+        content = "<span size=\"x-large\"><b>Run an Application</b></span>\\n<span size=\"small\">Remember to take breaks. Drink your water.</span>\\n\\n\\n\\n\\n\\n\\n\\n\\n<span><i>Alt+1: Workspaces\\nAlt+2: Applications(default)\\nAlt+3: All Executables\\nAlt+4: Connect to Server (SSH)</i></span>";
       };
+
 
       searchbox = {
         background-color = mkLiteral "${background-solid}";
@@ -65,18 +62,20 @@ in
         color = mkLiteral "${foreground}";
         border-color = mkLiteral "${blue_accent}";
         border = mkLiteral "0 0 1px 0";
-
+        padding = mkLiteral "0px 0px 0px 2px";
         expand = false;
       };
 
       listview = {
         background-color = mkLiteral "${background-alt}";
         color = mkLiteral "${foreground}";
+        expand = false;
       };
 
       element-text = {
         background-color = mkLiteral "${background-alt}";
         color = mkLiteral "${foreground}";
+        padding = mkLiteral "0px 0px 0px 2px";
       };
 
       "element-text selected" = {
@@ -86,4 +85,3 @@ in
     };
   };
 }
-
