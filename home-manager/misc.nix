aecca9c71b80e194e39c4e4c260160da5e985087
garaiza-93 <araizagustavo93@gmail.com>
2022-12-31 11:08:14 -0600

    mpris-proxy add

--- home-manager/misc.nix
+++ home-manager/misc.nix
@@ -7,6 +7,13 @@
     mpv.enable = true;
   };
 
+  systemd.user.services.mpris-proxy = {
+    Unit.Description = "Mpris proxy";
+    Unit.After = [ "network.target" "sound.target" ];
+    Service.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
+    Install.WantedBy = [ "default.target" ];
+  };
+
   home.packages = with pkgs; [
     ani-cli
     betterdiscordctl
