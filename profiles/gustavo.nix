6198111d7d3dc9a832c672a459c855ac5bdaef82
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2023-02-03 15:42:25 -0600

    import devtools and shell

--- profiles/gustavo.nix
+++ profiles/gustavo.nix
@@ -5,5 +5,10 @@
   manual.manpages.enable = true;
 
   imports = [
+   ../devtools/neovim/neovim.nix
+   ../devtools/git.nix
+   ../devtools/tmux.nix
+   ../common/btop.nix
+   ../common/zsh.nix
   ];
 }
