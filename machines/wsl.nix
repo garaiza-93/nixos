f52a08adc967dd594fd749f90063df3a8fc6b7de
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2023-02-03 15:42:06 -0600

    use older system version for wsl compatability

--- machines/wsl.nix
+++ machines/wsl.nix
@@ -1,15 +1,58 @@
-{ self, ... }:
+{ self, pkgs, ... }:
 
 {
-  imports = [
-    ../configuration.nix
-  ];
+#  imports = [
+#    ../configuration.nix
+#  ];
+
+  nix.package = pkgs.nixFlakes;
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  system.stateVersion = "22.11";
+
+  networking.hostName = "wsl";
+
+  time.timeZone = "America/Chicago";
+  i18n.defaultLocale = "en_US.utf8";
+
+  environment = {
+    shells = with pkgs; [ zsh ];
+    pathsToLink = [ "/libexec" ];
+    sessionVariables = {
+      XDG_CACHE_HOME = "\${HOME}/.cache";
+      XDG_CONFIG_HOME = "\${HOME}/.config";
+      XDG_BIN_HOME = "\${HOME}/.local/bin";
+      XDG_DATA_HOME = "\${HOME}/.local/share";
+      # following line adds to PATH, not replace.
+      PATH = [
+        "\${XDG_BIN_HOME}"
+      ];
+    };
+  };
+
+  users = {
+    defaultUserShell = pkgs.zsh;
+    users.gustavo = {
+      isNormalUser = true;
+      description = "gustavo";
+      extraGroups = [ "wheel" ];
+    };
+  };
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
 
   wsl = {
     enable = true;
+    wslConf.automount.root = "/mnt";
     defaultUser = "gustavo";
+    
     startMenuLaunchers = true;
-    nativeSystemd = true;
-    interop.register = false;
+    docker-native.enable = true;
   };
 }
