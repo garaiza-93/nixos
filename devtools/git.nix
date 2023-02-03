f5f0436b670cf09f1212bb7ae1990dea55611950
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2023-02-03 15:41:22 -0600

    import git config to nix

--- devtools/git.nix
+++ devtools/git.nix
@@ -3,5 +3,21 @@
 {
   programs.git = {
     enable = true;
+    userName = "Gustavo Araiza";
+    userEmail = "57430880+garaiza-93@users.noreply.github.com";
+    extraConfig = {
+      user.username = "garaiza-93";
+      init.defaultBranch = "master";
+      
+      core.editor = "nvim";
+      core.whitespace = "fix,-indent-with-non-tab,tailing-space,cr-at-eol";
+
+      web.browser = "firefox";
+
+      github.user = "garaiza-93";
+      github.token = "token";
+
+      url."ssh://git@github.com/".insteadOf = "https://github.com";
+    };
   };
 }
