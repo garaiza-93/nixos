3c1a917c7e0210cc135500347548f71161d648b3
garaiza-93 <araizagustavo93@gmail.com>
2023-02-09 20:43:47 -0600

    fix alias pointing to wrong file

--- common/zsh.nix
+++ common/zsh.nix
@@ -13,7 +13,7 @@ in
       l = "ls -lAth --color=auto";
       fucknvidia = "sudo nvidia-settings";
       sysedit = "nvim ~/.config/nixos/flake.nix";
-      nvimedit = "nvim ~/.config/nixos/devtools/neovim/config/neovim.nix";
+      nvimedit = "nvim ~/.config/nixos/devtools/neovim/neovim.nix";
       buildpersonal = "cd ~/.config/nixos && sudo nixos-rebuild switch --flake .#EVA-01";
       buildwork = "cd ~/.config/nixos && sudo nixos-rebuild switch --flake .#wsl";
       updatepersonal = "cd ~/.config/nixos && nix flake update && sudo nixos-rebuild switch --flake .#EVA-01";
