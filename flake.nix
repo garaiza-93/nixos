ddb8bcf8e1f98b8e2124642c2fa188d607d7315e
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2023-02-16 10:10:28 -0600

    fix wsl output

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, nixos-wsl, home-manager, polybar-master }:
+  outputs = { self, nixpkgs, nixpkgs-old, nixos-wsl, home-manager, polybar-master }:
     let
       system = "x86_64-linux";
     in
