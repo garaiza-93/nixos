7e7e5efde4c56a793f482fa495202a76550f212e
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2023-02-17 11:08:55 -0600

    use tokyo-night-gtk from nixpkgs

--- ui/x11/gtk.nix
+++ ui/x11/gtk.nix
@@ -1,11 +1,11 @@
-{ tokyo-night-gtk, ... }:
+{ pkgs, ... }:
 
 {
   gtk = {
     enable = true;
     theme = {
       name = "Tokyonight-Dark-B";
-      package = tokyo-night-gtk;
+      package = pkgs.tokyo-night-gtk;
     };
   };
 }
