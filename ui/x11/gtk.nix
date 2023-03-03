70b6a0765d0f8f05e3cb20ae46586f8b71a9829a
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2023-03-02 18:38:16 -0600

    change gtk theme, update bookmarks

--- ui/x11/gtk.nix
+++ ui/x11/gtk.nix
@@ -4,8 +4,17 @@
   gtk = {
     enable = true;
     theme = {
-      name = "Tokyonight-Dark-B";
+      name = "Tokyonight-Dark-BL-LB";
       package = pkgs.tokyo-night-gtk;
     };
+
+    gtk3.bookmarks = [
+      "file:///home/goose/Documents"
+      "file:///home/goose/Downloads"
+      "file:///home/goose/Games"
+      "file:///home/goose/Pictures"
+      "file:///home/goose/Videos"
+      "file:///home/goose/Torrents"
+    ];
   };
 }
