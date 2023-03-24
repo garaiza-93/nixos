834536851c945365a3f67213448330f91dc241f5
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2023-03-24 08:02:43 -0500

    add outputs to build/run project, use yarn and specific node version

--- templates/node-ts/flake.nix
+++ templates/node-ts/flake.nix
@@ -1,16 +1,25 @@
 {
-  description = "nodejs and Typescript devshell/builds using dream2nix";
+  description = "Basic Node+Typescript environment with language server";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    yarn2nix = {
+      url = "github:nix-community/yarn2nix";
+      flake = false;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
+  outputs = { nixpkgs, flake-utils, yarn2nix, ... }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
+
+          pname = "name-me";
+          version = "0.1.0";
+
           nvimrc = ''local servers = { 'eslint', 'tsserver' }
             local caps = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities());
 
@@ -23,15 +32,28 @@
         in
         rec
         {
+          # Executed by 'nix build'
+          packages.default = yarn2nix.mkYarnPackage {
+            inherit pname;
+            src = ./.;
+            yarnLock = ./yarn.lock;
+            packageJSON = ./package.json;
+          };
+
+          # Executed by `nix run`
+          apps.default = flake-utils.lib.mkApp { drv = packages.default; };
+
           # Used by `nix develop`
           devShells.default = pkgs.mkShell {
-
             buildInputs = with pkgs; [
+              nodejs-18_x
+              yarn
+
               nodePackages.typescript
               nodePackages.typescript-language-server
               nodePackages.vscode-langservers-extracted
-              nodejs
             ];
+
             shellHook = ''echo '${nvimrc}' > .nvimrc.lua'';
           };
         }
