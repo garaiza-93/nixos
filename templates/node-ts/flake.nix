1d9fb92fadb6c2d30611c3c529828f5a9700e700
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2023-03-24 07:11:58 -0500

    package nvim settings into flake, greatly simplify

--- templates/node-ts/flake.nix
+++ templates/node-ts/flake.nix
@@ -3,48 +3,37 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    dream2nix.url = "github:nix-community/dream2nix";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, dream2nix, flake-utils, ... }:
-    let
-      lib = nixpkgs.lib // builtins;
-      systems = [ "x86_64-linux" ];
-      forAllSystems = f:
-        lib.genAttrs systems (
-          system:
-          f system (nixpkgs.legacyPackages.${system})
-        );
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          nvimrc = ''local servers = { 'eslint', 'tsserver' }
+            local caps = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities());
 
-      partial-flake = dream2nix.lib.makeFlakeOutputs
+            for _, lsp in ipairs(servers) do
+              require('lspconfig')[lsp].setup { capabilities = caps }
+            end
+
+            vim.cmd("LspStart");
+            '';
+        in
+        rec
         {
-          systems = flake-utils.lib.defaultSystems;
-          config.projectRoot = ./.;
-          source = ./.;
-          projects = ./projects.toml;
-        };
-    in
-    dream2nix.lib.dlib.mergeFlakes [
-      partial-flake
-      {
-        devShells = forAllSystems (system: pkgs: (
-          lib.optionalAttrs
-            (partial-flake ? devShells.${system}.name-me.overrideAttrs)
-            rec {
-              default = name-me;
-              name-me =
-                partial-flake.devShells.${system}.name-me.overrideAttrs
-                  (old: {
-                    buildInputs = with pkgs; [
-                      nodePackages.typescript
-                      nodePackages.typescript-language-server
-                      nodePackages.vscode-langservers-extracted
-                      nodejs
-                    ] ++ old.buildInputs;
-                  });
-            }
-        ));
-      }
-    ];
+          # Used by `nix develop`
+          devShells.default = pkgs.mkShell {
+
+            buildInputs = with pkgs; [
+              nodePackages.typescript
+              nodePackages.typescript-language-server
+              nodePackages.vscode-langservers-extracted
+              nodejs
+            ];
+            shellHook = ''echo '${nvimrc}' > .nvimrc.lua'';
+          };
+        }
+      );
 }
