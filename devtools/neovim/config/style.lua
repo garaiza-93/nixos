b7af847b7bc4eb16b57218acbbe4fbb1fbd19cfc
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2023-04-23 13:13:59 -0500

    fix duplicate binding

--- devtools/neovim/config/style.lua
+++ devtools/neovim/config/style.lua
@@ -47,7 +47,7 @@ dashboard.section.header.opts.hl = purple
 dashboard.section.buttons.val = {
   dashboard.button('e', '  > New file', ':ene <BAR> startinsert <CR>'),
   dashboard.button('f', '  > Find file', ':cd $HOME | Telescope find_files<CR>'),
-  dashboard.button('f', '  > Find Text (grep)', 'Telescope live_grep<CR>'),
+  dashboard.button('g', '  > Find Text (grep)', 'Telescope live_grep<CR>'),
   dashboard.button('r', '  > Recent', ':Telescope oldfiles<CR>'),
   dashboard.button('c', '⚙  > Configuration', ':cd $HOME/.config/nvim | :e init.lua<CR>'),
   dashboard.button('q', '  > Quit NVIM', ':qa<CR>'),
