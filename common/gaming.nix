3c26fec66bb829d387c15e7e43eb86cacd6f4e08
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2023-05-27 19:00:37 -0500

    add osu

--- common/gaming.nix
+++ common/gaming.nix
@@ -2,6 +2,7 @@
 {
   home.sessionVariables."STEAM_EXTRA_COMPAT_TOOLS_PATHS" = "\${HOME}/.steam/root/compatibilitytools.d";
   home.packages = with pkgs; [
+    osu-lazer
     pcsx2
     prismlauncher-qt5
     protontricks
