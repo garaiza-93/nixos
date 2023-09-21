c4a114cf2ddf1f3c57e46423924eb48eb5d295c0
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2023-09-21 15:45:59 -0500

    add retroarch with some cores

--- common/gaming.nix
+++ common/gaming.nix
@@ -1,12 +1,18 @@
 { pkgs, ... }:
-{
-  home.sessionVariables."STEAM_EXTRA_COMPAT_TOOLS_PATHS" = "\${HOME}/.steam/root/compatibilitytools.d";
+let
+  retroarch = pkgs.retroarch.override {
+    cores = with pkgs.libretro; [ dolphin pcsx-rearmed mupen64plus ];
+  };
+in {
+  home.sessionVariables."STEAM_EXTRA_COMPAT_TOOLS_PATHS" =
+    "\${HOME}/.steam/root/compatibilitytools.d";
   home.packages = with pkgs; [
     osu-lazer
     pcsx2
     prismlauncher-qt5
     protontricks
     protonup-ng
+    retroarch
     steam
     steam-run
   ];
