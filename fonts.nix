f0e91876ced3ce94b0b834535cc09fdfce4a3806
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2023-12-06 12:22:23 -0600

    font config changes

--- fonts.nix
+++ fonts.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  fonts.fonts = with pkgs; [
+  fonts.packages = with pkgs; [
     (nerdfonts.override { fonts = [ "Hack" ]; })
     iosevka-comfy.comfy
     iosevka-comfy.comfy-wide
