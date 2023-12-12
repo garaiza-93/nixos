6a99aa37af80cb1641e163ba6517296d49e7983c
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2023-12-11 18:50:12 -0600

    switch out betterdiscord for dorion

--- common/user-apps.nix
+++ common/user-apps.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     ani-cli
-    betterdiscordctl
     discord
+    dorion
     gimp
     hue-plus
     libreoffice
@@ -11,4 +11,14 @@
     wineWowPackages.full
     winetricks
   ];
+  services.gammastep = {
+    enable = true;
+    dawnTime = "8:00-9:00";
+    duskTime = "17:00-18:00";
+    latitude = "34.0";
+    longitude = "-118";
+    tray = true;
+    settings.general.brightness-day = 1.0;
+    settings.general.brightness-night = 0.5;
+  };
 }
