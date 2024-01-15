ee1aa8924ae0c19dd9fce624345f88a87a2cb625
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2024-01-14 20:18:00 -0600

    patch mupen, add gaming goods

--- common/gaming.nix
+++ common/gaming.nix
@@ -1,19 +1,31 @@
-{ pkgs, ... }:
+{ pkgs, dolphin-emu-nix, steamtinkerlaunch-master, ... }:
 let
+  mupen64plus-patched = pkgs.libretro.mupen64plus.overrideAttrs (o: {
+    patches = (o.patches or [ ]) ++ [ ../patches/larger_cart_rom.patch ];
+  });
   retroarch = pkgs.retroarch.override {
-    cores = with pkgs.libretro; [ dolphin pcsx-rearmed mupen64plus flycast ];
+    cores = with pkgs.libretro; [
+      dolphin
+      pcsx-rearmed
+      mupen64plus-patched
+      flycast
+    ];
   };
 in {
   home.sessionVariables."STEAM_EXTRA_COMPAT_TOOLS_PATHS" =
     "\${HOME}/.steam/root/compatibilitytools.d";
   home.packages = with pkgs; [
+    dolphin-emu-nix.packages.x86_64-linux.dolphin-emu
+    itch
     osu-lazer
     pcsx2
     prismlauncher-qt5
     protontricks
     protonup-ng
+    r2modman
     retroarch
     steam
     steam-run
+    (steamtinkerlaunch.overrideAttrs (o: { src = steamtinkerlaunch-master; }))
   ];
 }
