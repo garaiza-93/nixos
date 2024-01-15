6e7e061eeb84339cd241627cd933dbb103bc311e
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2024-01-14 20:18:12 -0600

    inputs for gaming stuff, update flake

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
     home-manager-old.url = "github:nix-community/home-manager/release-22.11";
     nixos-wsl.url = "github:nix-community/NixOS-WSL";
     nvim-nixified.url = "github:garaiza-93/nvim-nixified";
+    dolphin-emu-nix.url = "github:matthewcroughan/dolphin-emu-nix";
 
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     home-manager-old.inputs.nixpkgs.follows = "nixpkgs";
@@ -17,10 +18,17 @@
       submodules = true;
       flake = false;
     };
+
+    steamtinkerlaunch-master = {
+      type = "git";
+      url = "https://github.com/sonic2kk/steamtinkerlaunch.git";
+      flake = false;
+    };
   };
 
   outputs = { self, nixpkgs, nixpkgs-old, nixos-wsl, home-manager
-    , home-manager-old, polybar-master, nvim-nixified }:
+    , home-manager-old, polybar-master, nvim-nixified, dolphin-emu-nix
+    , steamtinkerlaunch-master }:
     let system = "x86_64-linux";
     in {
       nixosConfigurations = {
@@ -34,7 +42,8 @@
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.extraSpecialArgs = {
-                inherit polybar-master nvim-nixified;
+                inherit polybar-master nvim-nixified dolphin-emu-nix
+                  steamtinkerlaunch-master;
               };
               home-manager.users.goose = {
                 imports = [ ./profiles/goose.nix ];
