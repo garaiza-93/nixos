603150d4e6702eb50aa1bbc99f2f6ade89b3293a
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2024-05-24 18:48:02 -0500

    rework rust template

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -3,68 +3,50 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-root.url = "github:srid/flake-root";
-    mission-control.url = "github:Platonic-Systems/mission-control";
     nvim-nixified.url = "github:garaiza-93/nvim-nixified";
   };
 
-  outputs =
-    inputs@{ self, nixpkgs, flake-utils, flake-parts, nvim-nixified, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = nixpkgs.lib.systems.flakeExposed;
-      imports =
-        [ inputs.flake-root.flakeModule inputs.mission-control.flakeModule ];
-      perSystem = { pkgs, lib, config, inputs', ... }:
-        let
-          pname = "name-me";
-          version = "0.1.0";
-
-          nvim-rust = inputs'.nvim-nixified.packages.rust-config;
-        in rec {
-          # Executed by `nix build`
-          packages.default = pkgs.rustPlatform.buildRustPackage {
-            inherit pname version;
-            src = ./.;
-            cargoLock.lockFile = ./Cargo.lock;
-          };
-
-          # Executed by `nix run`
-          apps.default = flake-utils.lib.mkApp { drv = packages.default; };
-
-          # Used by `nix develop`
-          mission-control.scripts = {
-            rWatch = {
-              description = "continuous cargo check";
-              exec = "boxxy -d cargo watch";
-            };
-            rWatchAll = {
-              description = "Watch check and tests";
-              exec = "boxxy -d cargo watch -x check -x test";
-            };
-            rTest = {
-              description = "Run tests";
-              exec = "boxxy -d cargo test";
-            };
-          };
-          devShells.default = pkgs.mkShell {
-            inputsFrom = [ config.mission-control.devShell ];
-            buildInputs = with pkgs; [
-              boxxy
-              nvim-rust
-              cargo
-              cargo-watch
-              rustc
-              clippy
-              rustfmt
-              rust-analyzer
-              pkg-config
-            ];
-
-            RUST_SRC_PATH =
-              "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-          };
+  outputs = { nixpkgs, flake-utils, fenix, nvim-nixified, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ fenix.overlays.default ];
         };
-    };
+
+        toolchain = pkgs.fenix.complete;
+        nvim-rust = nvim-nixified.packages.${system}.rust-config;
+      in rec {
+        # Executed by `nix build`
+        packages.default = (pkgs.makeRustPlatform {
+          inherit (toolchain) cargo rustc;
+        }).buildRustPackage {
+          pname = "template";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+
+        # Executed by `nix run`
+        apps.default = flake-utils.lib.mkApp { drv = packages.default; };
+
+        # Used by `nix develop`
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (with toolchain; [ cargo rustc rust-src clippy rustfmt ])
+            pkg-config
+            cargo-watch
+            rust-analyzer-nightly
+
+            nvim-rust
+          ];
+
+          RUST_SRC_PATH = "${toolchain.rust-src}/lib/rustlib/src/rust/library";
+        };
+      });
 }
