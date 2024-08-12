d7bb1fddd7b7a41ff696b1d2af482968e0ee1d69
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2024-08-12 09:50:14 -0500

    nurrrr

--- configuration.nix
+++ configuration.nix
@@ -8,4 +8,10 @@
     '';
   };
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.packageOverrides = pkgs: {
+    nur = import (builtins.fetchTarball
+      "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+        inherit pkgs;
+      };
+  };
 }
