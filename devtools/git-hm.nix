a2549891b9fac267e429c77df930017f03d3fa6e
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2024-09-10 10:55:20 -0500

    git config

---
+++ devtools/git-hm.nix
@@ -0,0 +1,27 @@
+{
+  programs.git = {
+    enable = true;
+    userName = "Gustavo Araiza";
+    userEmail = "57430880+garaiza-93@users.noreply.github.com";
+    aliases = {
+      fuckit = "push --force";
+      new = "checkout -b";
+      undo = "reset HEAD~1";
+    };
+    ignores = [ "/.data" "/.direnv" ".nvimrc.lua" "boxxy*" "*.envrc" ];
+    extraConfig = {
+      user.username = "garaiza-ntst";
+      init.defaultBranch = "master";
+
+      core.editor = "nvim";
+      core.whitespace = "fix,-indent-with-non-tab,tailing-space,cr-at-eol";
+
+      web.browser = "firefox";
+
+      github.user = "garaiza_ntst";
+      github.token = "token";
+
+      url."ssh://git@github.com".insteadOf = "https://github.com";
+    };
+  };
+}
