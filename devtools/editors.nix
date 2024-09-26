419f63d44838c27fe5bf8e341349f17338a7b822
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2024-09-26 11:57:56 -0500

    wrap helix with necessary tools, still wip

--- devtools/editors.nix
+++ devtools/editors.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, inputs, ... }: {
+{ pkgs, lib, config, inputs, ... }: {
   home.packages = [ inputs.nvim-nixified.packages.x86_64-linux.default ];
 
   programs.helix.enable = true;
@@ -7,17 +7,74 @@
     config.lib.file.mkOutOfStoreSymlink
     "${config.xdg.configHome}/nixos/devtools/helix-config.toml";
 
-  programs.helix.languages = {
-    language-server.omnisharp = {
-      command = "dotnet";
-      args = [
-        "${pkgs.omnisharp-roslyn}/lib/omnisharp-roslyn/OmniSharp.dll"
-        "--language-server"
+  programs.helix.package = pkgs.helix.overrideAttrs (self: {
+    makeWrapperArgs = with pkgs;
+      self.makeWrapperArgs or [ ] ++ [
+        "--suffix"
+        "PATH"
+        ":"
+        (lib.makeBinPath [
+          lldb
+          netcoredbg
+          dotnet-sdk
+          msbuild
+          ripgrep
+
+          taplo
+          marksman
+
+          nodePackages.bash-language-server
+          nil
+          omnisharp-roslyn
+          (python3.withPackages (ps:
+            with ps;
+            [ python-lsp-server ]
+            ++ python-lsp-server.optional-dependencies.all))
+          nodePackages.vscode-langservers-extracted
+          nodePackages.yaml-language-server
+        ])
+        "--set-default"
+        "RUST_SRC_PATH"
+        "${rustPlatform.rustcSrc}/library"
       ];
+  });
+
+  programs.helix.languages = with pkgs; {
+    language-server = {
+      nil = {
+        command = "${nil}/bin/nil";
+        # command = "nil";
+        config.nil = {
+          formatting.command = [ "${nixpkgs-fmt}/bin/nixpkgs-fmt" ];
+          nix.flake.autoEvalInputs = true;
+        };
+      };
+      omnisharp = {
+        command = "omnisharp";
+        args = [ "-l" "Error" "--languageserver" "-z" ];
+      };
     };
-    languages = [{
-      name = "c-sharp";
-      debugger.command = "netcoredbg";
-    }];
+    language = [
+      {
+        name = "c-sharp";
+        # debugger.name = "netcorecdg";
+        # debugger.command = "netcoredbg";
+        language-servers = [ "omnisharp" ];
+      }
+      {
+        name = "json";
+        language-servers = [{
+          name = "vscode-json-language-server";
+          except-features = [ "format" ];
+        }];
+      }
+      {
+        name = "markdown";
+        language-servers = [{
+          name = "marksman";
+          except-features = [ "format" ];
+        }];
+      }
+    ];
   };
 }
