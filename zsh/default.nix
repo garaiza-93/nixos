ac90853985d60208108097fa44b86ee5cdd3f4ec
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2024-10-12 12:10:30 -0500

    new env var NIX_CONFIG, use in aliases

--- zsh/default.nix
+++ zsh/default.nix
@@ -19,21 +19,21 @@ in {
       updateinput = ''
         nix flake metadata | grep -E "^├|^└" | sed 's/:.*//g' | sed -e 's/\x1b\[[0-9;]*m//g' | sed 's/[^a-zA-Z0-9\-]//g' | fzf | xargs nix flake lock --update-input'';
       buildpersonal =
-        "sudo nixos-rebuild switch --flake path:$HOME/.config/nixos#EVA-01 && cd -";
+        "sudo nixos-rebuild switch --flake path:$NIX_CONFIG#EVA-01 && cd -";
       buildwork =
-        "sudo nixos-rebuild switch --flake path:$HOME/.config/nixos#wsl && cd -";
+        "sudo nixos-rebuild switch --flake path:$NIX_CONFIG#wsl && cd -";
       buildhm =
-        "home-manager switch --flake $HOME/.config/nixos#gustavo-hm && cd -";
+        "home-manager switch --flake $NIX_CONFIG#gustavo-hm && cd -";
       updatepersonal =
         "cd ~/.config/nixos && nix flake update && sudo nixos-rebuild switch --flake .#EVA-01 && cd -";
       updatework =
         "cd ~/.config/nixos && nix flake update && sudo nixos-rebuild switch --flake .#wsl && cd -";
       updatehm =
-        "cd ~/.config/nixos && nix flake update && home-manager switch --flake $HOME/.config/nixos#gustavo-hm && cd -";
+        "cd ~/.config/nixos && nix flake update && home-manager switch --flake $NIX_CONFIG#gustavo-hm && cd -";
       updatenvim = "cd ~/.config/nixos && nix flake update nvim-nixified";
       neofetch = "neofetch --source ${img}/seele-ascii";
-      gitexcludefile = "$HOME/.config/nixos/scripts/gitexcludefile.sh";
-      updateflakes = "$HOME/.config/nixos/scripts/update_flakes.sh";
+      gitexcludefile = "$NIX_CONFIG/scripts/gitexcludefile.sh";
+      updateflakes = "$NIX_CONFIG/scripts/update_flakes.sh";
       fuck-it-ssh = "eval $(ssh-agent -s) && ssh-add";
     };
     history = {
@@ -54,6 +54,7 @@ in {
     envExtra = ''
       export TERM=xterm-256color
       export DIRENV_LOG_FORMAT=
+      export NIX_CONFIG=$XDG_CONFIG_HOME/nixos
       eval "$(direnv hook zsh)"
     '';
   };
